61c13557272835479b21e3af227fa248b050eb29
kiranKamath <k96white@gmail.com>
2020-02-28 17:13:06 +0530

    take to top button added

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -37,6 +37,8 @@ function searchValue(e){
                 else{
                     btnText.innerHTML="Dark Mode";
                 }
+                
+                
             }
 
 
@@ -81,3 +83,24 @@ async function fetchNews(searchUrl){
 }
 fetchNews("top-headlines?country=in"); //by default it fetch news related to india
 
+//Take to top functionality
+
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction();};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+document.getElementById("myBtn").addEventListener("click",topFunction);
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
